96b277f8b07e3633af4f76c3a6058284df11e0ac
@dewa.awidiya <dewa.awidiya@shopee.com>
2023-05-21 11:33:25 +0700

    finish demo

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"sikm-marketplace-db/db"
 	"sikm-marketplace-db/repository"
 )
+
+func PrintToJSON(data any) {
+	b, _ := json.Marshal(data)
+	fmt.Println(string(b))
+}
 
 func main() {
 	db, err := db.Connect(db.DBCredential{
@@ -22,5 +28,42 @@ func main() {
 
 	fmt.Println("connected to DB")
 
-	_ = repository.NewProductRepo(db)
+	productRepo := repository.NewProductRepo(db)
+
+	// products, err2 := productRepo.FindAll()
+	// if err2 != nil {
+	// 	fmt.Println("failed to get all products", err2)
+	// }
+	// PrintToJSON(products)
+
+	// p, err3 := productRepo.FindByID(5)
+	// if err3 != nil {
+	// 	fmt.Println("failed get product id", err3)
+	// }
+	// PrintToJSON(p)
+
+	// err4 := productRepo.Save(&model.Product{
+	// 	Name:       "Topi Keren",
+	// 	Price:      55000,
+	// 	CategoryID: 3,
+	// })
+	// if err4 != nil {
+	// 	fmt.Println("error insert", err4)
+	// }
+
+	// err5 := productRepo.Update(5, &model.Product{
+	// 	Name:       "Topi Keren Updated",
+	// 	Price:      45000,
+	// 	CategoryID: 3,
+	// })
+	// if err5 != nil {
+	// 	fmt.Println("failed to update", err5)
+	// }
+
+	// JOIN example
+	pc, err6 := productRepo.FindDetail(4)
+	if err6 != nil {
+		fmt.Println("failed to get prd detail", err6)
+	}
+	PrintToJSON(pc)
 }
--- repository/product.go
+++ repository/product.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"sikm-marketplace-db/model"
 )
 
@@ -10,6 +12,7 @@ type ProductRepo interface {
 	FindByID(id int) (*model.Product, error)
 	Save(product *model.Product) error
 	Update(id int, product *model.Product) error
+	FindDetail(id int) (*model.ProductCategory, error)
 }
 
 type productRepoImpl struct {
@@ -23,17 +26,58 @@ func NewProductRepo(db *sql.DB) *productRepoImpl {
 }
 
 func (r *productRepoImpl) FindAll() ([]model.Product, error) {
-	return []model.Product{}, nil
+	rows, err := r.db.Query("select id, name, price, category_id from products")
+	if err != nil {
+		return []model.Product{}, err
+	}
+	defer rows.Close()
+
+	products := []model.Product{}
+	for rows.Next() {
+		var item model.Product
+		rows.Scan(&item.ID, &item.Name, &item.Price, &item.CategoryID)
+		products = append(products, item)
+	}
+
+	return products, nil
 }
 
 func (r *productRepoImpl) FindByID(id int) (*model.Product, error) {
-	return nil, nil
+	row := r.db.QueryRow("select id, name, price, category_id from products where id = $1", id)
+	if row.Err() != nil {
+		return nil, row.Err()
+	}
+	var item model.Product
+	err := row.Scan(&item.ID, &item.Name, &item.Price, &item.CategoryID)
+	// check sentinel error not found
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, errors.New("product not found")
+	}
+	return &item, row.Err()
 }
 
 func (r *productRepoImpl) Save(product *model.Product) error {
-	return nil
+	// todo: teman2 bisa pake result untuk cek rowsAffected
+	_, err := r.db.Exec("INSERT INTO products (name, price, category_id) VALUES ($1, $2, $3)", product.Name, product.Price, product.CategoryID)
+	return err
 }
 
 func (r *productRepoImpl) Update(id int, product *model.Product) error {
-	return nil
+	_, err := r.db.Exec("UPDATE products SET name=$1, price=$2, category_id=$3 WHERE id=$4", product.Name, product.Price, product.CategoryID, id)
+	return err
+}
+
+func (r *productRepoImpl) FindDetail(id int) (*model.ProductCategory, error) {
+	row := r.db.QueryRow("SELECT p.id, p.name, p.price, p.category_id, c.name FROM products p JOIN categories c ON c.id = p.category_id WHERE p.id = $1", id)
+	var prd model.ProductCategory
+	if row.Err() != nil {
+		fmt.Println("failed to get")
+	}
+	if err := row.Scan(&prd.ID, &prd.Name, &prd.Price, &prd.CategoryID, &prd.CategoryName); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errors.New("product not found")
+		}
+		return nil, err
+	}
+	return &prd, nil
 }
